565b593d4cf6aeae57cb20aad2c9801e1e1dfbca
Flavio Nogueira <flavionogueirabarros@gmail.com>
2023-05-20 13:22:01 -0300

    fixing controller adding @body parameter

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import CreateEmailDto from './dto/create.email.dto';
 
@@ -7,7 +7,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('email')
-  async buildemail(createDto: CreateEmailDto): Promise<string> {
+  async buildemail(@Body() createDto: CreateEmailDto): Promise<string> {
     return await this.appService.buildEmailservice(createDto);
   }
 }
